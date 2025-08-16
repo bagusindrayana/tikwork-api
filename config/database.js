6bad69f74ebda83bb69b8dea412e094912d291eb
bagusindrayana <bagusindrayanaindo@gmail.com>
2025-08-16 15:48:06 +0800

    database config

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('db_tikwork', 'root', '', {
-  host: 'localhost',
+const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, {
+  host: process.env.DATABASE_HOST,
   dialect: 'mysql',
   logging: false
 });
